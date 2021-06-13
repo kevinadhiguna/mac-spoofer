38802b196fcbb5f510f9cd2dae193af7f1004cad
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-13 14:22:57 +0700

    refactor: set a new MAC address dinamically

--- macspoofer.py
+++ macspoofer.py
@@ -3,11 +3,16 @@ import subprocess
 # Name of interface
 interface = "wlan0"
 
-# Run 'ifconfig wlan0 down' command in shell to deactivate a network interface named wlan0
-subprocess.call("ifconfig wlan0 down", shell=True)
+# Set a new MAC address
+new_mac_address = "00:11:22:33:44:66"
 
-# Changing MAC address
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
+print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
 
-# Activate wlan0 network interface
-subprocess.call("ifconfig wlan0 up", shell=True)
+# # Run 'ifconfig wlan0 down' command in shell to deactivate a network interface named wlan0
+# subprocess.call("ifconfig wlan0 down", shell=True)
+
+# # Changing MAC address
+# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
+
+# # Activate wlan0 network interface
+# subprocess.call("ifconfig wlan0 up", shell=True)
