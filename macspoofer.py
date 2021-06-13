f6062070c2472fc50d9e2f01ea38f8e2920f986d
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-13 14:12:57 +0700

    feat: change MAC address statically

--- macspoofer.py
+++ macspoofer.py
@@ -1,4 +1,10 @@
 import subprocess
 
-# Run 'ifconfig' command in shell
-subprocess.call("ifconfig", shell=True)
+# Run 'ifconfig wlan0 down' command in shell to deactivate a network interface named wlan0
+subprocess.call("ifconfig wlan0 down", shell=True)
+
+# Changing MAC address
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
+
+# Activate wlan0 network interface
+subprocess.call("ifconfig wlan0 up", shell=True)
