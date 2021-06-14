e784263e01deb6dd113240829e0789528e1ebb94
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-14 13:51:26 +0700

    refactor: create ga et_argumments function

--- macspoofer.py
+++ macspoofer.py
@@ -1,35 +1,26 @@
 import subprocess
 import optparse
 
-# Create an instance for the OptionParser
-parser = optparse.OptionParser()
+def get_arguments():
+  """
+  A function to get arguments.
+  """
+  # Create an instance for the OptionParser
+  parser = optparse.OptionParser()
 
-parser.add_option(
-  "-i", "--interface",  # A parameter which will be initialized before storing a network interface name.
-  dest="interface",     # Store the value that comes after '-i' or '--interface' to a variable called 'interface'.
-  help="A network interface name whose MAC address will be changed" # If a user needs help. please display this to explain what '-i' and '--interface' are for.
-)
+  parser.add_option(
+    "-i", "--interface",  # A parameter which will be initialized before storing a network interface name.
+    dest="interface",     # Store the value that comes after '-i' or '--interface' to a variable called 'interface'.
+    help="A network interface name whose MAC address will be changed" # If a user needs help. please display this to explain what '-i' and '--interface' are for.
+  )
+  parser.add_option(
+    "-m", "--mac",              # A parameter which will be initialized before storing a new MAC address.
+    dest="new_mac_address",     # Store the value that comes after '-m' or '--mac' to a variable called 'new_mac_address'.
+    help="A new MAC address"    # If a user needs help. please display this to explain what '-m' and '--mac' are for.
+  )
 
-parser.add_option(
-  "-m", "--mac",              # A parameter which will be initialized before storing a new MAC address.
-  dest="new_mac_address",     # Store the value that comes after '-m' or '--mac' to a variable called 'new_mac_address'.
-  help="A new MAC address"    # If a user needs help. please display this to explain what '-m' and '--mac' are for.
-)
-
-# 'parse_args()' will return options(e.g.: -i, --mac) and arguments(e.g.: 'wlan0', '00:11:22:33:44:55')
-(options, arguments) = parser.parse_args()
-
-# Set name of interface to 'dest' in interface option
-interface = options.interface
-
-# Set a new MAC address to 'dest' in new MAC address option
-new_mac_address = options.new_mac_address
-
-# -- for Python 2.7 --
-# interface = raw_input("network interface > ")
-# new_mac_address = raw_input("new MAC address >")
-
-print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
+  # 'parse_args()' will return options(e.g.: -i, --mac) and arguments(e.g.: 'wlan0', '00:11:22:33:44:55')
+  return parser.parse_args()
 
 def spoof_mac_address(interface, new_mac_address):
   """
@@ -43,3 +34,8 @@ def spoof_mac_address(interface, new_mac_address):
   
   # Activate wlan0 network interface
   subprocess.call(["ifconfig", interface, "up"])
+
+(options, argumemnts) = get_arguments()
+
+# Call the function to change MAC address and passing the required arguments
+spoof_mac_address(options.interface, options.new_mac_address)
